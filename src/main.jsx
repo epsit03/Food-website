0329573d219c81d6f579c0bf983abd7255ed1a28
aditi3007 <aditirdc@gmail.com>
2025-08-02 00:16:54 +0530

    Food-ordering-website

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Food-ordering-website/">
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
